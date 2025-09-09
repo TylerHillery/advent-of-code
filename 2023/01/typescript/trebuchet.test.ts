a3eff1c609bfda00e624d38565e3d0b377dcc695
Tyler Hillery <tyhillery@gmail.com>
2025-09-09 14:25:21 -0700

    feat(2023-day01-part02-typescript): add solution

--- 2023/01/typescript/trebuchet.test.ts
+++ 2023/01/typescript/trebuchet.test.ts
@@ -1,9 +1,10 @@
+import { readFile } from "node:fs/promises";
 import path from "node:path";
 
 import { fileURLToPath } from "url";
-import { describe, expect,it } from "vitest";
+import { describe, expect, it } from "vitest";
 
-import { part1 } from "./trebuchet";
+import { part1, part2 } from "./trebuchet";
 
 describe("Day 1: Trebuchet?!", () => {
   it("Part 1", async () => {
@@ -14,8 +15,24 @@ describe("Day 1: Trebuchet?!", () => {
       "data",
       "example_input_part_1.txt",
     );
-    const result = await part1(inputFile);
+    const data = await readFile(inputFile, "utf-8");
+    const result = part1(data);
     const expectedResult = 142;
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("Part 2", async () => {
+    const inputFile = path.join(
+      fileURLToPath(import.meta.url),
+      "..",
+      "..",
+      "data",
+      "example_input_part_2.txt",
+    );
+
+    const data = await readFile(inputFile, "utf-8");
+    const result = part2(data);
+    const expectedResult = 281;
     expect(result).toEqual(expectedResult);
   });
 });
--- 2023/01/typescript/trebuchet.ts
+++ 2023/01/typescript/trebuchet.ts
@@ -2,9 +2,7 @@ import { readFile } from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-export async function part1(file: string): Promise<number> {
-  const data = await readFile(file, "utf-8");
-
+export function part1(data: string): number {
   function getFirstAndLastDigit(str: string): number {
     const digits = str.split("").filter((char) => !isNaN(Number(char))); // could also use a regex str.match(/\d/g)
     const firstDigit = digits[0];
@@ -22,9 +20,56 @@ export async function part1(file: string): Promise<number> {
     .reduce((sum, cailbrationValue) => sum + cailbrationValue, 0);
 }
 
+export function part2(data: string): number {
+  function getFirstAndLastDigit(str: string) {
+    const validDigits = {
+      one: "1",
+      two: "2",
+      three: "3",
+      four: "4",
+      five: "5",
+      six: "6",
+      seven: "7",
+      eight: "8",
+      nine: "9",
+    };
+    let firstDigit: string | undefined = undefined;
+    let lastDigit: string | undefined = undefined;
+
+    for (let i = 0; i < str.length; i++) {
+      if (!isNaN(Number(str[i]))) {
+        if (firstDigit === undefined) {
+          firstDigit = str[i];
+        }
+        lastDigit = str[i];
+        continue;
+      }
+      for (const [k, v] of Object.entries(validDigits)) {
+        const strDigit = str.slice(i, i + k.length);
+        if (strDigit === k) {
+          if (firstDigit === undefined) {
+            firstDigit = v;
+          }
+          lastDigit = v;
+          break;
+        }
+      }
+    }
+
+    return firstDigit && lastDigit ? Number(firstDigit + lastDigit) : 0;
+  }
+
+  return data
+    .split("\n")
+    .map((line) => getFirstAndLastDigit(line))
+    .reduce((sum, value) => sum + value, 0);
+}
+
 if (import.meta.url === `file://${process.argv[1]}`) {
   const filename = fileURLToPath(import.meta.url);
   const inputFilePath = path.join(filename, "..", "..", "data", "input.txt");
+  const data = await readFile(inputFilePath, "utf-8");
 
-  console.log("Answer to part 1:", await part1(inputFilePath));
+  console.log("Answer to part 1:", part1(data));
+  console.log("Answer to part 2:", part2(data));
 }
