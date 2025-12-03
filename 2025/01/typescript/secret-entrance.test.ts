eef13f1985e854b3f30781faa658ca3af1efa08e
Tyler Hillery <tyhillery@gmail.com>
2025-12-03 11:36:38 -0600

    feat(2025-day01-part02-typescript): add solution

--- 2025/01/typescript/secret-entrance.test.ts
+++ 2025/01/typescript/secret-entrance.test.ts
@@ -3,7 +3,7 @@ import path from "node:path";
 
 import { expect, test } from "vitest";
 
-import { part1 } from "./secret-entrance";
+import { part1, part2 } from "./secret-entrance";
 
 test("Part 1", async () => {
   const data = await readFile(
@@ -12,5 +12,15 @@ test("Part 1", async () => {
   );
   const result = part1(data);
   const expected = 3;
+  expect(result).toEqual(expected);
+});
+
+test("Part 2", async () => {
+  const data = await readFile(
+    path.join(import.meta.dirname, "..", "data", "example_input_part_2.txt"),
+    "utf-8",
+  );
+  const result = part2(data);
+  const expected = 6;
   expect(result).toEqual(expected);
 });
--- 2025/01/typescript/secret-entrance.ts
+++ 2025/01/typescript/secret-entrance.ts
@@ -38,8 +38,43 @@ export function part1(input: string): number {
 }
 
 export function part2(input: string): number {
-  console.log(input);
-  return 42;
+  let counter = 0;
+  input.split("\n").reduce((acc, line) => {
+    const direction = line[0];
+    if (!isDirection(direction)) {
+      throw new Error(
+        `Not a valid direction must be one of: ${DIRECTIONS.join()}`,
+      );
+    }
+    const distance = Number(line.slice(1));
+
+    let temp: number;
+
+    switch (direction) {
+      case "L":
+        temp = acc - distance;
+        // if we start at 0 determines weather or not we should count that first crossing point
+        if (acc !== 0) {
+          counter += Math.abs(Math.floor(temp / 100));
+        } else {
+          counter += Math.floor(Math.abs(temp / 100));
+        }
+        acc = temp % 100;
+        if (acc < 0) {
+          acc += 100;
+        }
+        if (acc === 0) counter++;
+        break;
+      case "R":
+        temp = acc + distance;
+        counter += Math.floor(temp / 100);
+        acc = temp % 100;
+        break;
+    }
+    return acc;
+  }, 50);
+
+  return counter;
 }
 
 async function main() {
@@ -49,6 +84,9 @@ async function main() {
   );
   const part1Answer = part1(data);
   console.log(`Answer to Part 1: ${part1Answer}`);
+
+  const part2Answer = part2(data);
+  console.log(`Answer to Part 2: ${part2Answer}`);
 }
 
 main();
